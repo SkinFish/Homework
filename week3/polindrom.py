0796af9234da53aba25a7c67dd4a1524acd10e7e
SkinFish <skinfish@skinfish.com>
2017-09-26 17:09:51 +0300

    Added homework for week 3

---
+++ week3/polindrom.py
@@ -0,0 +1,11 @@
+def is_polindrom(text):
+    if len(text) > 100:
+        return "Text is longer than 100 characters"
+    text = str(text)
+    text = text.lower()
+    #text = text.replace(" ","")
+    rev_text = text[::-1]
+    if text == rev_text:
+        return True
+    else:
+        return False
